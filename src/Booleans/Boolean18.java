cb3e01a3a8d3dc1edd5e8bc2535f485f510c3f86
Javohirbek-Xujamurodov <XujamurodovJavohirbek2007@gmail.com>
2025-02-14 18:51:02 +0500

    core

---
+++ src/Booleans/Boolean18.java
@@ -0,0 +1,20 @@
+package Booleans;
+
+import java.util.Scanner;
+
+public class Boolean18 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(" Integers A : ");
+        int a = scanner.nextInt();
+        System.out.print(" Integers B : ");
+        int b = scanner.nextInt();
+        System.out.print(" Integers C : ");
+        int c = scanner.nextInt();
+
+        System.out.println( (a == b ) || ( b == c) || ( c == a ));
+    }
+}
+
+
