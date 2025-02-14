2031677ac1e3c2e5d502fe202b3ca364a49f184a
Javohirbek-Xujamurodov <XujamurodovJavohirbek2007@gmail.com>
2025-02-14 18:51:02 +0500

    core

---
+++ src/Booleans/Boolean15.java
@@ -0,0 +1,21 @@
+package Booleans;
+
+import java.util.Scanner;
+
+public class Boolean15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(" Integers A : ");
+        int a = scanner.nextInt();
+
+        System.out.print(" Integers B : ");
+        int b = scanner.nextInt();
+        System.out.print(" Integers C : ");
+        int c = scanner.nextInt();
+
+        System.out.println( (a > 0 && b > 0 && c <= 0) || (a > 0 && b <= 0 && c > 0) || (a <= 0 && b > 0 && c > 0));
+    }
+}
+
+
